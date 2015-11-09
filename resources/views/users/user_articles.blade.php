79ff98d09a136fc5a393dc7a2dd0ab8cb645dcb3
Alexander Litvinenko <litvinenko1706@gmail.com>
2015-11-09 16:12:28 +0200

    ~ sort user articles by date

--- resources/views/users/user_articles.blade.php
+++ resources/views/users/user_articles.blade.php
@@ -3,7 +3,7 @@
 @section('user_info')
 
 @if (count($user->articles))
-    @include('articles._list', ['articles' => $user->articles])
+    @include('articles._list', ['articles' => $user->articles->sortByDesc('created_at')])
 @else
     User has no articles
 @endif
