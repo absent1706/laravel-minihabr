f042bd3e40bdd9d6dca889176f03c8a659325852
Alexander Litvinenko <litvinenko1706@gmail.com>
2015-11-14 19:17:15 +0200

    +1 TODO (remove data getting from categories list partial)

--- resources/views/categories/_list.blade.php
+++ resources/views/categories/_list.blade.php
@@ -4,6 +4,7 @@
     <h4 class="panel-title">Categories</h4>
   </div>
   <div class="list-group">
+    {{-- TODO: remove getting data in partial (see laravel fundamentals 25 - When You Want a View Partial to Always Receive Data.mp4) --}}
     @foreach (App\Category::all() as $category)
         {!! link_to_route('articles.index', $category->name, ['cat' => $category->id],
                           ['class' => 'list-group-item'.(($category->id == $category_id) ? ' active' : '')]
