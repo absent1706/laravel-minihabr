3e6f0754519ad63475beb1f32c26e2c2ce8d8b36
Alexander Litvinenko <litvinenko1706@gmail.com>
2015-12-12 00:34:17 +0200

    ~

--- resources/views/articles/_list.blade.php
+++ resources/views/articles/_list.blade.php
@@ -1,10 +1,10 @@
-<div class="list-unstyled list-split-hr">
+<ul class="list-unstyled list-split-hr">
     @foreach ($articles as $article)
         <li>
             @include('articles._single', ['display_full_body' => false])
         </li>
     @endforeach
-</div>
+</ul>
 
 @if (auth()->check())
     @if(config('frontend.allow_ajax_crud_requests'))
